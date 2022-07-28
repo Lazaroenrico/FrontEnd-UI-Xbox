4bc39dcf8518cb6c9231ccf193f12f615659d616
Lázaro Enrico <lazaro.enr.menezes@gmail.com>
2022-07-28 01:18:03 -0300

    feature: color and font settings

--- src/assets/styles/colors.ts
+++ src/assets/styles/colors.ts
@@ -1,6 +1,7 @@
 export const colors = {
     primaryBgColor: '0FFFFF',
     secondBgColor: '#E9ECEF',
+    buttonCreUpColor:'#2284A3',
 
     titleColor: '#0FFFFF',
 
--- src/assets/styles/mixins.ts
+++ src/assets/styles/mixins.ts
@@ -2,11 +2,56 @@ import { css } from "styled-components";
 import { colors } from "./colors";
 import { constants } from "./constants";
 
+
+
 const titleStyle = () => css`
   font-family: ${constants.titleFontFamily};
   font-size: ${constants.titleFontSize};
 `;
 
+const ButtonAqua = () => css`
+  width: 124px;
+  height: 48px;
+  border-radius: 10px;
+  background-color: ${colors.buttonCreUpColor};
+  font-family: ${constants.textFontFamily};
+  font-weight: bold;
+  font-size: ${constants.textFontSize};
+  text-align: center;
+  cursor: pointer;
+  transition: all ease-in-out 0.6s;
+    :hover {
+      transform: scale(1.1);
+  }
+`
+const ButtonCancel = () => css`
+  width: 130px;
+  height: 48px;
+  border-radius: 10px;
+  background-color: #A32222;
+  font-family: ${constants.textFontFamily};
+  font-weight: bold;
+  font-size: ${constants.textFontSize};
+  text-align: center;
+  cursor: pointer;
+  transition: all ease-in-out 0.6s;
+    :hover {
+      transform: scale(1.1);
+  }
+`
+
+const titleModal = () => css`
+text-align: center;
+font-family: ${constants.titleFontFamily};
+font-size: ${constants.titleFontSize};
+color: ${colors.titleColor};
+`
+
+
 export const mixins = {
- titleStyle,
+  titleStyle,
+  ButtonAqua,
+  ButtonCancel,
+  titleModal,
+
 }
