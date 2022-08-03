04664a6147d20a0dc9c2fc9015a1aec8eab41852
Lázaro Enrico <lazaro.enr.menezes@gmail.com>
2022-08-02 23:17:07 -0300

    fix(tyoes): implament index(interface)

---
+++ src/types/index.tsx
@@ -0,0 +1,11 @@
+export interface Product {
+    id?: string;
+    name: string;
+    description: string;
+    price: number;
+    image: string;
+    categoryId: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+  }
+  
